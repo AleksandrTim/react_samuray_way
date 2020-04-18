8f2cee2746c1c76d164236730fb22664b28faed8
alex_tim <aleksandr.tim707@gmail.com>
2020-04-19 00:54:44 +0300

    63 DAL, axios.create

---
+++ src/api/api.js
@@ -0,0 +1,18 @@
+import * as axios from 'axios';
+
+const instance = axios.create({
+    withCredentials: true
+    , baseURL: 'https://social-network.samuraijs.com/api/1.0'
+    , headers: {"API-KEY": "78de4df0-e8f0-40fa-b6ad-a9d4fd807306"}
+})
+
+export const usersAPI = {
+    getUsers(currentPage, pageSize) {
+        return instance.get(`/users?page=${currentPage}&count=${pageSize}`,
+            {
+                withCredentials: true
+            })
+            .then(response => response.data); 
+    }
+}
+
