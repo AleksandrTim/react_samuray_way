abbe56a7f687d5b3d40ae0db1bbc6992b5c0b1d1
alex_tim <aleksandr.tim707@gmail.com>
2020-04-20 02:17:45 +0300

    67 redux-thunk 2.0

--- src/api/api.js
+++ src/api/api.js
@@ -2,23 +2,33 @@ import * as axios from 'axios';
 
 const instance = axios.create({
     withCredentials: true
-    , baseURL: 'https://social-network.samuraijs.com/api/1.0'
+    , baseURL: 'https://social-network.samuraijs.com/api/1.0/'
     , headers: { "API-KEY": "78de4df0-e8f0-40fa-b6ad-a9d4fd807306" }
 })
 
 export const usersAPI = {
     getUsers(currentPage, pageSize) {
-        return instance.get(`/users?page=${currentPage}&count=${pageSize}`,
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`,
             {
                 withCredentials: true
             })
             .then(response => response.data);
     }
     , follow(userId) {
-        return instance.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
+        return instance.post(`follow/${userId}`)
     }
     , unfollow(userId) {
-        return instance.delete(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
+        return instance.delete(`follow/${userId}`)
     }
+    , getProfile(userId) {
+        return instance.get(`profile/`+ userId)
+        
+    }
+}   
+
+export const authAPI = {
+    me() {
+        return instance.get(`auth/me`)
+    } 
 }
 
