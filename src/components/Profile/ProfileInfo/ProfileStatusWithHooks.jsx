c165203a425f3e8c7f22ef37e3036c3ed5f95414
alex_tim <aleksandr.tim707@gmail.com>
2020-04-25 02:52:10 +0300

    84 hooks, useState

---
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import s from './ProfileInfo.module.css';
+import { useState } from 'react';
+import { setStatus } from '../../../redux/profile-reducer';
+
+// let arr = [0, () => {}];
+// let [a, setA] = arr;
+
+const ProfileStatusWithHooks = (props) => {
+
+    // let stateWithSetState = useState(true);
+    // let editMode = stateWithSetState[0];
+    // let setEditMode = stateWithSetState[1];
+
+    let [editMode, setEditMode] = useState(false);
+    let [status, setStatus] = useState(props.status);
+
+    const activateEditMode = () => {
+        setEditMode(true);
+    }
+
+    const deactivatseEditMode = () => {
+        setEditMode(false); 
+    }
+
+    const onStatusChange = (e) => {
+        setStatus(e.currentTarget.value);
+    }
+
+    return (
+        <div>
+            {!editMode &&
+                <div>
+                    <span 
+                    onDoubleClick={activateEditMode}
+                    >
+                        {props.status || "-------"} yo
+                    </span>
+                </div>
+            }
+            {editMode &&
+                <div>
+                    <input
+                        // ref={this.statusInput}
+                        onChange={onStatusChange}
+                        autoFocus={true}
+                        onBlur={deactivatseEditMode}
+                        value={status}
+
+                    ></input>
+                </div>
+            }
+        </div>
+    )
+}
+
+export default ProfileStatusWithHooks;
