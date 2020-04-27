5483d6548dfe056505ea5069535d4cbb4e0b5af7
alex_tim <aleksandr.tim707@gmail.com>
2020-04-28 00:37:44 +0300

    Updates

---
+++ precache-manifest.d2acf00004b5ef270f2169829c6d2098.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da9413a38bc4c50a7955abd1f80971b7",
+    "url": "/react_samuray_way/index.html"
+  },
+  {
+    "revision": "d7636d716955762d59df",
+    "url": "/react_samuray_way/static/css/3.1d3e5bb2.chunk.css"
+  },
+  {
+    "revision": "a8d193b21288afb7a988",
+    "url": "/react_samuray_way/static/css/4.e89480ce.chunk.css"
+  },
+  {
+    "revision": "05abf9fff9d166add9a0",
+    "url": "/react_samuray_way/static/css/main.0ad8b15c.chunk.css"
+  },
+  {
+    "revision": "4218c7dbc39a0435d53c",
+    "url": "/react_samuray_way/static/js/2.37207aaa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react_samuray_way/static/js/2.37207aaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7636d716955762d59df",
+    "url": "/react_samuray_way/static/js/3.5738245d.chunk.js"
+  },
+  {
+    "revision": "a8d193b21288afb7a988",
+    "url": "/react_samuray_way/static/js/4.d3c2b9f1.chunk.js"
+  },
+  {
+    "revision": "05abf9fff9d166add9a0",
+    "url": "/react_samuray_way/static/js/main.75e5a58e.chunk.js"
+  },
+  {
+    "revision": "46bb042d2ba34ab4534c",
+    "url": "/react_samuray_way/static/js/runtime-main.bd8dcd32.js"
+  },
+  {
+    "revision": "6dd908bbbbf700b9e21bfc49956b8a6a",
+    "url": "/react_samuray_way/static/media/preloader.6dd908bb.svg"
+  }
+]);
