6d112dada7dcd14670819c492c3056a92007853e
alex_tim <aleksandr.tim707@gmail.com>
2020-05-03 17:08:34 +0300

    98 captcha for Login page + fix API

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ const instance = axios.create({
     withCredentials: true,
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     headers:     {
-        "API-KEY": "b1775b2f-c3a5-4509-8dc9-90b5629de7c3"
+        "API-KEY": "78de4df0-e8f0-40fa-b6ad-a9d4fd807306"
     }
 });
 
